1f4f20cccf07a4c753679069914088e91ab466dd
Eric Manongdo <eric@superscriptcreative.com>
2020-05-07 18:34:56 -1000

    Form Agreement

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -17,20 +17,20 @@ const SignUpForm = () => {
           </label>
         </li>
         <h6>Personal Information</h6>
-        <ul class="form-list">
-          <li class="form-list__row">
+        <ul className="form-list">
+          <li className="form-list__row">
             <label htmlFor="name">
               Name:
               <input id="name" type="text" name="name" required="true" />
             </label>
           </li>
-          <li class="form-list__row">
+          <li className="form-list__row">
             <label htmlFor="email">
               Email Address:
               <input id="email" type="text" name="email" required="true" />
             </label>
           </li>
-          <li class="form-list__row">
+          <li className="form-list__row">
             <label htmlFor="phone">
               Phone Number:
               <input id="phone" type="tel" name="phone" required="true" />
@@ -39,14 +39,14 @@ const SignUpForm = () => {
         </ul>
 
         <h6>Business Information</h6>
-        <ul class="form-list">
-          <li class="form-list__row">
+        <ul className="form-list">
+          <li className="form-list__row">
             <label htmlFor="bizname">
               Business Name:
               <input id="bizname" type="text" name="bizname" required="true" />
             </label>
           </li>
-          <li class="form-list__row">
+          <li className="form-list__row">
             <label htmlFor="bizaddress">
               Business Address:
               <input
@@ -57,19 +57,19 @@ const SignUpForm = () => {
               />
             </label>
           </li>
-          <li class="form-list__row">
+          <li className="form-list__row">
             <label htmlFor="bizphone">
               Phone Number:
               <input id="bizphone" type="tel" name="bizphone" required="true" />
             </label>
           </li>
-          <li class="form-list__row">
+          <li className="form-list__row">
             <label htmlFor="bizwebsite">
               Website:
               <input id="bizwebsite" type="tel" name="bizwebsite" required="" />
             </label>
           </li>
-          <li class="form-list__row">
+          <li className="form-list__row">
             <p style={{ fontSize: "0.7rem", marginBottom: "0" }}>
               Does your site need an update?
             </p>
@@ -85,32 +85,112 @@ const SignUpForm = () => {
               </label>
             </div>
           </li>
-          {/* <li class="form-list__row">
-            <label htmlFor="cars">
-              <p style={{ fontSize: "0.7rem" }}>
-                What POS System are you using?
-              </p>
-              <select id="cars" name="cars">
-                <option value="revel">revel</option>
-                <option value="lightspeed">lightspeed</option>
-                <option value="parpixelpoint">par pixel point</option>
-                <option value="nextep">nextep</option>
-                <option value="uniwell">uniwell</option>
-                <option value="toast">toast</option>
-                <option value="micros">micros</option>
-                <option value="na">n/a</option>
-                <option value="other">other</option>
-              </select>
-            </label>
-          </li> */}
-          <li class="form-list__row">
+          <li className="form-list__row">
             <label htmlFor="checks">
               What address would you like monthly checks to be mailed to?
               <input id="checks" type="text" name="checks" required="" />
             </label>
           </li>
+          <li className="form-list__row">
+            <p style={{ fontSize: "0.7rem", marginBottom: "0" }}>
+              Do you agree to all terms and conditions?
+            </p>
+            <p className="contract">
+              Terms and Conditions for grindz to go Introduction These Website
+              Standard Terms and Conditions written on this webpage shall manage
+              your use of our website, grindz partner accessible at
+              grindzpartner.com. These Terms will be applied fully and affect to
+              your use of this Website. By using this Website, you agreed to
+              accept all terms and conditions written in here. You must not use
+              this Website if you disagree with any of these Website Standard
+              Terms and Conditions. These Terms and Conditions have been
+              generated with the help of the Terms And Conditions Template and
+              the Terms and Conditions Generator. Minors or people below 18
+              years old are not allowed to use this Website. Intellectual
+              Property Rights Other than the content you own, under these Terms,
+              grindz to go and/or its licensors own all the intellectual
+              property rights and materials contained in this Website. You are
+              granted limited license only for purposes of viewing the material
+              contained on this Website. Restrictions You are specifically
+              restricted from all of the following: publishing any Website
+              material in any other media; selling, sublicensing and/or
+              otherwise commercializing any Website material; publicly
+              performing and/or showing any Website material; using this Website
+              in any way that is or may be damaging to this Website; using this
+              Website in any way that impacts user access to this Website; using
+              this Website contrary to applicable laws and regulations, or in
+              any way may cause harm to the Website, or to any person or
+              business entity; engaging in any data mining, data harvesting,
+              data extracting or any other similar activity in relation to this
+              Website; using this Website to engage in any advertising or
+              marketing. Certain areas of this Website are restricted from being
+              access by you and grindz to go may further restrict access by you
+              to any areas of this Website, at any time, in absolute discretion.
+              Any user ID and password you may have for this Website are
+              confidential and you must maintain confidentiality as well. Your
+              Content In these Website Standard Terms and Conditions, "Your
+              Content" shall mean any audio, video text, images or other
+              material you choose to display on this Website. By displaying Your
+              Content, you grant grindz to go a non-exclusive, worldwide
+              irrevocable, sub licensable license to use, reproduce, adapt,
+              publish, translate and distribute it in any and all media. Your
+              Content must be your own and must not be invading any
+              third-party’s rights. grindz to go reserves the right to remove
+              any of Your Content from this Website at any time without notice.
+              Your Privacy Please read Privacy Policy. No warranties This
+              Website is provided "as is," with all faults, and grindz to go
+              express no representations or warranties, of any kind related to
+              this Website or the materials contained on this Website. Also,
+              nothing contained on this Website shall be interpreted as advising
+              you. Limitation of liability In no event shall grindz to go, nor
+              any of its officers, directors and employees, shall be held liable
+              for anything arising out of or in any way connected with your use
+              of this Website whether such liability is under contract. grindz
+              to go, including its officers, directors and employees shall not
+              be held liable for any indirect, consequential or special
+              liability arising out of or in any way related to your use of this
+              Website. Indemnification You hereby indemnify to the fullest
+              extent grindz to go from and against any and/or all liabilities,
+              costs, demands, causes of action, damages and expenses arising in
+              any way related to your breach of any of the provisions of these
+              Terms. Severability If any provision of these Terms is found to be
+              invalid under any applicable law, such provisions shall be deleted
+              without affecting the remaining provisions herein. Variation of
+              Terms grindz to go is permitted to revise these Terms at any time
+              as it sees fit, and by using this Website you are expected to
+              review these Terms on a regular basis. Assignment The grindz to go
+              is allowed to assign, transfer, and subcontract its rights and/or
+              obligations under these Terms without any notification. However,
+              you are not allowed to assign, transfer, or subcontract any of
+              your rights and/or obligations under these Terms. Entire Agreement
+              These Terms constitute the entire agreement between grindz to go
+              and you in relation to your use of this Website, and supersede all
+              prior agreements and understandings. Governing Law & Jurisdiction
+              These Terms will be governed by and interpreted in accordance with
+              the laws of the State of us, and you submit to the non-exclusive
+              jurisdiction of the state and federal courts located in us for the
+              resolution of any disputes.
+            </p>
+
+            <br />
+            <div style={{ display: "flex" }}>
+              <label htmlFor="agree">
+                agree
+                <input type="radio" id="agree" name="terms" value="agree" />
+              </label>
+              <label htmlFor="disagree">
+                No
+                <input
+                  type="radio"
+                  id="disagree"
+                  name="terms"
+                  value="disagree"
+                />
+              </label>
+            </div>
+          </li>
           <li>
-            <button type="submit" class="button">
+            <button type="submit" className="button">
               sign up
             </button>
           </li>
@@ -146,7 +226,13 @@ const FormWrapper = styled.div`
     position: absolute;
     width: 1px;
   } */
-
+  .contract {
+    height: 100px;
+    overflow: scroll;
+    font-size: 8px;
+    line-height: 0.8rem;
+    margin: 10px 0px;
+  }
   .button {
     color: #ffffff;
     background-color: var(--mainColor);
@@ -243,6 +329,7 @@ const FormWrapper = styled.div`
     text-transform: capitalize;
     font-weight: 400;
     font-size: 11px;
+    margin-right: 10px;
     color: var(--gray);
   }
   .form-list__row--inline {
